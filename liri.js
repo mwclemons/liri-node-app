60fab8f2f33c234bf981b99edd110e6cc3ef4745
Michael Clemons <mwclemons@gmail.com>
2019-05-09 16:21:54 -0500

    made runMyCommand recursive and added LogAndWrite fucntion

--- liri.js
+++ liri.js
@@ -14,33 +14,41 @@ var myQuery = ""
 for (var x = 3; x < process.argv.length; x++) {
     myQuery = myQuery + process.argv[x] + " ";
 };
-myQuery = myQuery.trim();
 
-if (myCommand === "do-what-it-says") {
-    fs.readFile("random.txt", "utf8", function(error, data) {
-        // If the code experiences any errors it will log the error to the console.
-        if (error) {
-            return console.log(error);
-        }
+fs.appendFileSync("log.txt", "\n\n"+"$ node liri.js "+myCommand+" "+myQuery, function(err) {
 
-        var dataArr = data.split(",");
-        myCommand = dataArr[0];
-        myQuery = dataArr[1];
-        runMyCommand();
-    });
-} else {
-    runMyCommand();
-}
+    // If the code experiences any errors it will log the error to the console.
+    if (err) {
+      return console.log(err);
+    }
+  
+});
+
+runMyCommand();
 
 function runMyCommand() {
-    if (myCommand === "concert-this") {
+    myQuery = myQuery.trim();
+
+    if (myCommand === "do-what-it-says") {
+        fs.readFile("random.txt", "utf8", function(error, data) {
+            // If the code experiences any errors it will log the error to the console.
+            if (error) {
+                return LogAndWrite(error);
+            }
+    
+            var dataArr = data.split(",");
+            myCommand = dataArr[0];
+            myQuery = dataArr[1];
+            runMyCommand();
+        });
+    } else if (myCommand === "concert-this") {
         axios.get("https://rest.bandsintown.com/artists/" + myQuery + "/events?app_id=codingbootcamp").then(
             function(res) {
                 for (var x = 0; x < res.data.length; x++) {
-                    console.log("Venue: " +res.data[x].venue.name);
-                    console.log("Venue Location: " +res.data[x].venue.city + ", " + res.data[x].venue.region + " " + res.data[x].venue.country);
-                    console.log("Date: " +moment(res.data[x].datetime).format("MM/DD/YYYY"));
-                    console.log("------------------------------------------------")
+                    LogAndWrite("Venue: " +res.data[x].venue.name);
+                    LogAndWrite("Venue Location: " +res.data[x].venue.city + ", " + res.data[x].venue.region + " " + res.data[x].venue.country);
+                    LogAndWrite("Date: " +moment(res.data[x].datetime).format("MM/DD/YYYY"));
+                    LogAndWrite("------------------------------------------------")
                 }
             }
         );
@@ -49,27 +57,38 @@ function runMyCommand() {
         spotify
             .search({ type: 'track', query: myQuery, limit: 1})
             .then(function(res) {
-                console.log(JSON.stringify(res.tracks.items[0].artists[0].name, null, 2));
-                console.log(JSON.stringify(res.tracks.items[0].name, null, 2));
-                console.log(JSON.stringify(res.tracks.items[0].preview_url, null, 2));
-                console.log(JSON.stringify(res.tracks.items[0].album.name, null, 2));
+                LogAndWrite(JSON.stringify(res.tracks.items[0].artists[0].name, null, 2));
+                LogAndWrite(JSON.stringify(res.tracks.items[0].name, null, 2));
+                LogAndWrite(JSON.stringify(res.tracks.items[0].preview_url, null, 2));
+                LogAndWrite(JSON.stringify(res.tracks.items[0].album.name, null, 2));
             })
             .catch(function(err) {
-                console.log(err);
+                LogAndWrite(err);
         });
     } else if (myCommand === "movie-this") {
         if (myQuery === "") {myQuery = "Mr. Nobody"}
         axios.get("http://www.omdbapi.com/?t="+myQuery+"&y=&plot=short&apikey=trilogy").then(
         function(res) {
-            console.log("Movie Title: " +res.data.Title);
-            console.log("Year: " +res.data.Year);
-            console.log("IMDB Rating: " +res.data.imdbRating);
-            console.log("Rotten Tomatoes Rating: " +res.data.Ratings[1].Value);
-            console.log("Country Produced: " +res.data.Country);
-            console.log("Language: " +res.data.Language);
-            console.log("Plot: " +res.data.Plot);
-            console.log("Actors: " +res.data.Actors);
+            LogAndWrite("Movie Title: " +res.data.Title);
+            LogAndWrite("Year: " +res.data.Year);
+            LogAndWrite("IMDB Rating: " +res.data.imdbRating);
+            LogAndWrite("Rotten Tomatoes Rating: " +res.data.Ratings[1].Value);
+            LogAndWrite("Country Produced: " +res.data.Country);
+            LogAndWrite("Language: " +res.data.Language);
+            LogAndWrite("Plot: " +res.data.Plot);
+            LogAndWrite("Actors: " +res.data.Actors);
         });
     }; 
 };    
 
+function LogAndWrite(myString) {
+    console.log(myString);
+    fs.appendFileSync("log.txt", "\n"+myString, function(err) {
+
+        // If the code experiences any errors it will log the error to the console.
+        if (err) {
+          return console.log(err);
+        }
+      
+    });
+}
